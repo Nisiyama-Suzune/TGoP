5a1a800ebf9cd34a997cc540def841c60743f4f5
Nisiyama-Suzune <Nisiyama_Suzune@icloud.com>
2017-06-30 13:01:55 +0800

    added LCT

--- src/string.cpp
+++ src/string.cpp
@@ -16,7 +16,7 @@ namespace string {
 				L / (L - 1 - fail[L - 1]) > 1, where L = match.size ().
 	*/
 
-	template <int MAXN = 1E6>
+	template <int MAXN = 1000000>
 	struct kmp {
 
 		std::string match;
@@ -55,10 +55,8 @@ namespace string {
 			state::dest : the automaton link.
 	*/
 
-	template <int MAXN = 1E6, int MAXC = 26>
+	template <int MAXN = 1000000, int MAXC = 26>
 	struct suffix_automaton {
-
-		state *head, *tail;
 
 		struct state {
 			int len;
@@ -66,15 +64,16 @@ namespace string {
 			state (int len = 0) : len (len), parent (NULL) {
 				memset (dest, 0, sizeof (dest));
 			}
-			state *extend (state *, int token);
 		} node_pool[MAXN * 2], *tot_node, *null = new state();
 
-		state *state::extend (state *start, int token) {
-			state *p = this;
-			state *np = this -> dest[token] ? null : new (tot_node++) state (this -> len + 1);
+		state *head, *tail;
+		
+		void extend (int token) {
+			state *p = tail;
+			state *np = tail -> dest[token] ? null : new (tot_node++) state (tail -> len + 1);
 			while (p && !p -> dest[token])
 				p -> dest[token] = np, p = p -> parent;
-			if (!p) np -> parent = start;
+			if (!p) np -> parent = head;
 			else {
 				state *q = p -> dest[token];
 				if (p -> len + 1 == q -> len) {
@@ -88,7 +87,7 @@ namespace string {
 					}
 				}
 			}
-			return np == null ? np -> parent : np;
+			tail = np == null ? np -> parent : np;
 		}
 
 		void init () {
@@ -98,10 +97,6 @@ namespace string {
 
 		suffix_automaton () {
 			init ();
-		}
-
-		void extend (int token) {
-			tail = tail -> extend (head, token);
 		}
 
 	};
@@ -118,7 +113,7 @@ namespace string {
 			node::len : the palindromic string length of the node.
 	*/
 
-	template <int MAXN = 1E6, int MAXC = 26>
+	template <int MAXN = 1000000, int MAXC = 26>
 	struct palindromic_tree {
 
 		struct node {
@@ -161,6 +156,59 @@ namespace string {
 
 		palindromic_tree () {
 			init ();
+		}
+
+	};
+
+	/*	Suffix Array :
+			Suffix array implementation.
+			void suffix_array::solve (int *a, int n) :
+				sa[i] : the beginning position of the ith smallest suffix.
+				rk[i] : the rank of the suffix beginning at position i.
+				height[i] :	the longest common prefix of sa[i] and sa[i - 1]. 
+	*/
+
+	template <int MAXN = 1000000, int MAXC = 26>
+	struct suffix_array {
+
+		int rk[MAXN], height[MAXN], sa[MAXN];
+		
+		int cmp (int *x, int a, int b, int d) {
+			return x[a] == x[b] && x[a + d] == x[b + d];
+		}
+		
+		void doubling (int *a, int n) {
+			static int sRank[MAXN], tmpA[MAXN], tmpB[MAXN];
+			int m = MAXC;
+			int *x = tmpA, *y = tmpB;
+			for (int i = 0; i < m; ++i) sRank[i] = 0;
+			for (int i = 0; i < n; ++i) ++sRank[x[i] = a[i]];
+			for (int i = 1; i < m; ++i) sRank[i] += sRank[i - 1];
+			for (int i = n - 1; i >= 0; --i) sa[--sRank[x[i]]] = i;
+			for (int d = 1, p = 0; p < n; m = p, d <<= 1) {
+				p = 0; for (int i = n - d; i < n; ++i) y[p++] = i;
+				for (int i = 0; i < n; ++i) if (sa[i] >= d) y[p++] = sa[i] - d;
+				for (int i = 0; i < m; ++i) sRank[i] = 0;
+				for (int i = 0; i < n; ++i) ++sRank[x[i]];
+				for (int i = 1; i < m; ++i) sRank[i] += sRank[i - 1];
+				for (int i = n - 1; i >= 0; --i) sa[--sRank[x[y[i]]]] = y[i];
+				std::swap (x, y); x[sa[0]] = 0; p = 1;
+				y[n] = -1;
+				for (int i = 1; i < n; ++i)
+					x[sa[i]] = cmp (y, sa[i], sa[i - 1], d) ? p - 1 : p++;
+			}
+		}
+		
+		void solve (int *a, int n) {
+			doubling (a, n);
+			for (int i = 0; i < n; ++i) rk[sa[i]] = i;
+			int cur = 0;
+			for (int i = 0; i < n; ++i)
+				if (rk[i]) {
+					if (cur) cur--;
+					for (; a[i + cur] == a[sa[rk[i] - 1] + cur]; ++cur);
+					height[rk[i]] = cur;
+				}
 		}
 
 	};
