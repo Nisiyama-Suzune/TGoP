5a850711e387676c59b7804210ee642c510603db
Nisiyama-Suzune <Nisiyama_Suzune@icloud.com>
2017-08-18 02:52:05 +0800

    fix

--- src/hack/hack.cpp
+++ src/hack/hack.cpp
@@ -13,7 +13,7 @@
 //	Optimizing hack :
 
 #define __ __attribute__ ((optimize ("-O3"))) 
-#define _ __ __inline __attribute__ ((__gnu_inline__, __always_inline__, __artificial__)) __attribute__ ((aligned))
+#define _ __ __inline __attribute__ ((__gnu_inline__, __always_inline__, __artificial__)) 
 #define NDEBUG
 
 //	Stack hack :
